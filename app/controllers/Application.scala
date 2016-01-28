803dd08bba9e34503b124150a1fa9048fe0332fd
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-01-28 15:18:27 +0900

    add markdown convert to html action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,23 @@
 package controllers
 
+import org.pegdown.PegDownProcessor
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index())
+  }
+
+  def convert = Action { implicit request =>
+    request.body.asFormUrlEncoded match {
+      case Some(data) =>
+        val markdown = data.getOrElse("markdown", Seq("")).head
+        val htmlOpt = Option(new PegDownProcessor().markdownToHtml(markdown))
+        Ok(htmlOpt.getOrElse(""))
+      case None =>
+        BadRequest("markdown not found")
+    }
   }
 
 }
