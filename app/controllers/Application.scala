d84ddd7ec84d537ac7626da3f69a7d16d7480b01
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-01-28 15:27:25 +0900

    fix convert action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,11 +12,15 @@ class Application extends Controller {
   def convert = Action { implicit request =>
     request.body.asFormUrlEncoded match {
       case Some(data) =>
-        val markdown = data.getOrElse("markdown", Seq("")).head
-        val htmlOpt = Option(new PegDownProcessor().markdownToHtml(markdown))
-        Ok(htmlOpt.getOrElse(""))
+        data.get("markdown") match {
+          case Some(markdown) =>
+            val htmlOpt = Option(new PegDownProcessor().markdownToHtml(markdown.head))
+            Ok(htmlOpt.getOrElse(""))
+          case None =>
+            BadRequest("markdown not found")
+        }
       case None =>
-        BadRequest("markdown not found")
+        BadRequest("invalid request parameter")
     }
   }
 
