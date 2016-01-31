7d43266fcb881d0f638ffa6e5e0ee17863a72434
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-01 01:01:07 +0900

    support convert to html

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,4 +3,15 @@ $(function () {
         const html = marked($(this).val());
         $('#preview').html(html);
     });
+
+    $('#a-html').click(function () {
+        $.ajax({
+            url: 'markdown',
+            type: 'POST',
+            data: {markdown: $('#editor').val()}
+        }).done(function (data) {
+            const dummy = $('<pre>').text(data);
+            $('#html').html(dummy);
+        });
+    });
 });
